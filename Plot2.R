79a182d0594cdff87f711501c47fbe0d31eab7e0
gotohtll <lim_leonhart@yahoo.com.sg>
2015-01-11 22:52:31 +0800

    Exploratory Data Analysis 1
    Exploratory Data Analysis 1

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+dataFile <- "C:/Users/Admin/R Files/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png(filename = "Plot2.png", width = 480, height = 480)
+dev.copy(png, file = "C:/Users/Admin/R Files/Plot2.png")
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
